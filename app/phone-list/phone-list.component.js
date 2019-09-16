047b53e8ecb555cff359089c9eaaa53457b565c2
moriokameda <morio.kameda@instavr.co>
2019-09-16 23:42:20 +0900

    6-step

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -4,20 +4,24 @@
 angular.
   module('phoneList').
   component('phoneList', {
-      // Note: The URL is relative to our `index.html` file
     templateUrl: 'phone-list/phone-list.template.html',
     controller: function PhoneListController() {
       this.phones = [
         {
           name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.'
+          snippet: 'Fast just got faster with Nexus S.',
+          age: 1
         }, {
           name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
+          snippet: 'The Next, Next Generation tablet.',
+          age: 2
         }, {
           name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
+          snippet: 'The Next, Next Generation tablet.',
+          age: 3
         }
       ];
+
+      this.orderProp = 'age';
     }
   });
