3c3d3b03ef9d5323c2c3c6e9b7ac9b8fc7e17998
moriokameda <morio.kameda@instavr.co>
2019-09-19 00:22:55 +0900

    8-step

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -10,7 +10,7 @@ angular.
       self.orderProp = 'age';
 
       $http.get('phones/phones.json').then(function(response) {
-        self.phones = response.data.slice(0,5);
+        self.phones = response.data;
       });
     }
   });
--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -20,6 +20,40 @@ describe('PhoneCat Application', function() {
       expect(phoneList.count()).toBe(8);
     });
 
+    it('should be possible to control phone order via the drop-down menu', function() {
+      var queryField = element(by.model('$ctrl.query'));
+      var orderSelect = element(by.model('$ctrl.orderProp'));
+      var nameOption = orderSelect.element(by.css('option[value="name"]'));
+      var phoneNameColumn = element.all(by.repeater('phone in $ctrl.phones').column('phone.name'));
+
+      function getNames() {
+        return phoneNameColumn.map(function(elem) {
+          return elem.getText();
+        });
+      }
+
+      queryField.sendKeys('tablet');   // Let's narrow the dataset to make the assertions shorter
+
+      expect(getNames()).toEqual([
+        'Motorola XOOM\u2122 with Wi-Fi',
+        'MOTOROLA XOOM\u2122'
+      ]);
+
+      nameOption.click();
+
+      expect(getNames()).toEqual([
+        'MOTOROLA XOOM\u2122',
+        'Motorola XOOM\u2122 with Wi-Fi'
+      ]);
+    });
+
+    it('should render phone specific links', function() {
+      var query = element(by.model('$ctrl.query'));
+      query.sendKeys('nexus');
+    
+      element.all(by.css('.phones li a')).first().click();
+      expect(browser.getCurrentUrl()).toContain('index.html#!/phones/nexus-s');
+    });
     
 
   });
