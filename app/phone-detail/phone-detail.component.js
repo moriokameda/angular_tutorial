91ceaf10934e17b59298c514ff76f53ea0c83ac3
moriokameda <morio.kameda@instavr.co>
2019-09-25 01:16:23 +0900

    12-step

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -6,9 +6,18 @@ angular.
       function PhoneDetailController($http, $routeParams) {
         var self = this;
 
+        self.setImage = function setImage(imageUrl) {
+            self.mainImageUrl = imageUrl;
+          };
+
         $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
           self.phone = response.data;
+          self.setImage(self.phone.images[0]);
         });
+
+        self.onDblclick = function onDblclick(imageUrl) {
+            alert('You double-clicked image: ' + imageUrl);
+          };
       }
     ]
   });
